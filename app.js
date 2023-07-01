3f7e33b562691b95fd0735cdc2e2c84ac9c8c548
abasha2002 <108979315+abasha2002@users.noreply.github.com>
2023-07-01 14:46:09 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const element = document.getElementById("num");
+let number = 0;
+
+setInterval(function add() {
+    if (number <= 20) {
+        element.innerHTML = number;
+        number++;
+    }
+}, 2000);
